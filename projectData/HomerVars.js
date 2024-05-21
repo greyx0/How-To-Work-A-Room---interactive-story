03af41f39f1d0bbe50a2f4225696aeddf4a13c2e
greyx0 <119888119+greyx0@users.noreply.github.com>
2024-05-21 13:20:47 +0200

    init

---
+++ projectData/HomerVars.js
@@ -0,0 +1,109 @@
+/**
+* javascript variables for the Homer project:
+* Grey Combattelli
+* Homer - The Story Flow Editor.
+* Copyright (c)2021-2024. Open Lab s.r.l - Florence, Italy
+* Developer: Pupunzi (Matteo Bicocchi)
+*
+**/
+
+class HomerVars {
+
+
+};
+
+class HomerActors {
+
+      static MATT_S_MOM = "Matt's Mom";
+      static RANDOM_GIRL__1 = "Random Girl #1";
+      static RANDOM_GUY__2 = "Random Guy #2";
+      static RANDOM_GIRL__2 = "Random Girl #2";
+      static RANDOM_GUY__1 = "Random Guy #1";
+      static NARRATOR = "Narrator";
+      static KIMBERLY = "Kimberly";
+      static MATT = "Matt";
+
+}
+
+class HomerMeta {
+      static FLOW_STATE = {
+          IDEA : 'IDEA',
+          NOTES : 'NOTES',
+          DRAFT : 'DRAFT',
+          EDITOR : 'EDITOR',
+          FINAL : 'FINAL',
+      };
+      static MOOD = {
+          NEUTRAL : 'NEUTRAL',
+          HAPPY : 'HAPPY',
+          VERY_HAPPY : 'VERY_HAPPY',
+          SAD : 'SAD',
+          ANGRY : 'ANGRY',
+          AMAZED : 'AMAZED',
+      };
+      static EXPRESSION = {
+          RELAXED : 'RELAXED',
+          GRINNING : 'GRINNING',
+          PENSIVE : 'PENSIVE',
+          FROWNING : 'FROWNING',
+          CRYING : 'CRYING',
+          ENRAGED : 'ENRAGED',
+      };
+      static BALLOON_TYPE = {
+          SPEECH : 'SPEECH',
+          WHISPER : 'WHISPER',
+          THOUGHT : 'THOUGHT',
+          SCREAM : 'SCREAM',
+          SLEEP : 'SLEEP',
+          SING_SONG : 'SING_SONG',
+      };
+      static LOCATION = {
+          HOME : 'HOME',
+          OUTSIDE : 'OUTSIDE',
+          OFFICE : 'OFFICE',
+          PARK : 'PARK',
+          SEA : 'SEA',
+          MOUNTAIN : 'MOUNTAIN',
+      };
+      static CAMERA_DIRECTION = {
+          CLOSE_UP : 'CLOSE_UP',
+          ANGLE_ON : 'ANGLE_ON',
+          OFF_SCREEN : 'OFF_SCREEN',
+          FADE_OUT : 'FADE_OUT',
+          FADE_IN : 'FADE_IN',
+          VOICE_OVER : 'VOICE_OVER',
+      };
+      static LIGHTNING = {
+          KEY : 'KEY',
+          BACK : 'BACK',
+          PRACTICAL : 'PRACTICAL',
+          HARD : 'HARD',
+          SOFT : 'SOFT',
+          CHIAROSCURO : 'CHIAROSCURO',
+      };
+
+}
+
+class HomerLabels {
+
+
+}
+
+
+class HomerFlowSlugs {
+      static _HOW_TO__WORK_A_ROOM_ = "(How To: Work A Room)";
+      static BONE_BAG = "Bone Bag";
+      static FARRINGTON_POND = "Farrington Pond";
+      static BLUE_AGE = "Blue Age";
+      static _HOW_TO__INITIATE_CONVERSATION_ = "(How To: Initiate Conversation)";
+      static COMES_AS_RELIEF = "Comes as Relief";
+      static SOME_BRIGHT_LIGHTS = "Some Bright Lights";
+      static A_GASH_ON_THE_CHEEK = "A Gash on the Cheek";
+      static YDS2M__YOU_DO_SOMETHING_TO_ME_ = "YDS2M (You Do Something To Me)";
+      static _HOW_TO__APPEAR_MORE_CONFIDENT_ = "(How To: Appear More Confident)";
+      static GO_TO_SLEEP_KIMBERLY = "Go To Sleep Kimberly";
+      static STOCKHOLM_SYNDROME = "Stockholm Syndrome";
+      static HOW_TO__MAKE_AN_EXIT = "How To: Make An Exit";
+      static SHAPELESS_PLACE = "Shapeless Place";
+ }
+
