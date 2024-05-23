f8fba0d5a6ead39522c5b5ac14b1f0ac37878630
greyx0 <119888119+greyx0@users.noreply.github.com>
2024-05-23 13:10:08 +0200

    revert

--- javascript/homer.js
+++ javascript/homer.js
@@ -399,3 +399,5 @@ window.homerDrawer = {
 		console.debug("THE END")
 	}
 };
+
+<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/40FZD9ladH2d3vmXtoLLQo?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
