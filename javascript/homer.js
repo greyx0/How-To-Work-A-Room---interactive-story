ddaba3120648e57aef337dea4543d34ae0fe4f41
greyx0 <119888119+greyx0@users.noreply.github.com>
2024-05-23 13:16:50 +0200

    ff

--- javascript/homer.js
+++ javascript/homer.js
@@ -399,15 +399,3 @@ window.homerDrawer = {
 		console.debug("THE END")
 	}
 };
-
-$(window).on('load resize', function() {
-	$('iframe[src*="embed.spotify.com"]').each( function() {
-	  $(this).css('width', $(this).parent().css('width'));
-	  $(this).attr('src', $(this).attr('src'));
-	  $(this).removeClass('loaded');
-	  
-	  $(this).on('load', function(){
-		$(this).addClass('loaded');
-	  });
-	});
-  });
